bf8d34c40d97c4aba6f257d7884ca8a1ddf1ab8c
Jessica Giraldo <jessicacgiraldo@gmail.com>
2023-07-26 17:29:03 -0400

    yeaaa we rule

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, make_response, jsonify
 from flask_cors import CORS
 from flask_migrate import Migrate
-
+from flask_restful import Api , Resource 
 from models import db, Message
 
 app = Flask(__name__)
@@ -14,9 +14,27 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+api = Api(app)
+
+
+class Messages(Resource):
+    def get(self):
+        messages = Message.query.order_by(Message.created_at).all()
+    
+        message_list = []
+        for message in messages:
+            message_dict = {
+                "id": message.id,
+                "body": message.body,
+                "username": message.username, 
+                "created_at": message.created_at,
+                "updated_at": message.updated_at
+            } 
+            message_list.append(message_dict)
+        response = make_response(message_list, 200)
+        return response
+
+api.add_resource(Messages, '/messages')
 
 @app.route('/messages/<int:id>')
 def messages_by_id(id):
